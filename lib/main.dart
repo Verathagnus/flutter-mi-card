67c39f67a077c812adc40ef933babf8af2d27aa0
FlutLab.io <>
2021-03-18 04:59:16 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -67,30 +67,6 @@ class MyApp extends StatelessWidget {
                   child: Row(
                     children: <Widget>[
                       Icon(
-                        Icons.phone,
-                        color: Colors.teal,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        'bishwaraj*@gmail.com',
-                        style: TextStyle(
-                          color: Colors.teal[900],
-                          fontFamily: 'Source Sans Pro',
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 25.0),
-                  padding: EdgeInsets.all(10.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
                         Icons.email,
                         color: Colors.teal,
                       ),
