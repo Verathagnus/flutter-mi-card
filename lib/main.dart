e78adace71507f57a3241f9a46edb3cbb5951b2a
FlutLab.io <>
2021-03-18 08:28:40 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,7 @@ class MyApp extends StatelessWidget {
                 ),
                 SizedBox(
                   height: 20.0,
+                  width: 150.0,
                   child: Divider(color: Colors.teal[50], thickness: 1.0,),
                 ),
                 Card(
